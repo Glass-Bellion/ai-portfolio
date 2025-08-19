4448c39a042031a3a376cfe69605bc58977311c4
Glass-Bellion <jvalentinbills@gmail.com>
2025-08-19 15:10:25 -0400

    day 2: python basics

---
+++ basics.py
@@ -0,0 +1,13 @@
+a = 3
+b = 4
+print(a+b)
+nums = [1,2,3]
+for n in nums:
+    print(n*n)
+
+person = {"name":"you", "likes":["tea","data"]}
+print(person["likes"][0])
+
+def square(x):
+    return x*x
+print(square(9))
